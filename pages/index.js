ccca8bc26c7476997583cc555bb4b031132d2f44
Mostafiz <mostafiz93@gmail.com>
2023-04-02 03:52:26 +0600

    version added

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+	console.log('version - 1.0.0');
 	return (
 		<div className={styles.bgimg}>
 			<Head>
