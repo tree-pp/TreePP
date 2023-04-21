fec9a50b142635460823690ff2b469b87eec7d95
Moudud Hasan <moudud99@gmail.com>
2023-04-22 03:59:31 +0600

    trying pull request

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-
+// comment for pull request
 export default function Home() {
 	return (
 		<div className={styles.bgimg}>
