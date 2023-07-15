e85cc0f8e1a4c6c40c645edf8a9a5ae06d34f3b7
Mostafiz <mostafiz93@gmail.com>
2023-07-15 15:42:28 +0600

    upd> literature

--- components/howItWorks.js
+++ components/howItWorks.js
@@ -9,12 +9,23 @@ export default function HowItWorks() {
 				How Tree++ Works
 			</Typography>
 			<p>
-				Next steps In this quickstart, you deployed a Kubernetes cluster and
-				then deployed a sample multi-container application to it. To learn more
-				about AKS by walking through a complete example, including building an
-				application, deploying from Azure Container Registry, updating a running
-				application, and scaling and upgrading your cluster, continue to the
-				Kubernetes cluster tutorial.
+				The objective of Tree++ is to play an instrumental role in achieving net
+				zero. Doing so it primarily offers carbon offsetting through tree
+				plantation.
+			</p>
+			<p>
+				In its digital platform any individual can calculate his carbon
+				footprint and may purchase carbon offset accordingly. In the same
+				platform the land owners who have unused plantation-ready lands may
+				enlist them. Tree++ brings the unique opportunity to the youth segment
+				to join the green movement to address global climate change by offering
+				hands-on efforts and get incentivised for their effort at the same time.
+				For corporations of any size Tree++ is the best place to offset their
+				carbon emission.
+			</p>
+			<p>
+				Thus, this novel platform connects all the stakeholders involved in
+				carbon offsetting through tree plantation in a win-win-win manner.
 			</p>
 		</Container>
 	);
