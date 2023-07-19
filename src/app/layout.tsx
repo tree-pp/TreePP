5a3202ddff5ebcae2fa52df27310a2514fe0ae10
moudud99 <moudud99@gmail.com>
2023-07-19 21:55:13 +0600

    fixing routing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Navbar from '../Components/Navbar'
+import Navbar from '../components/Navbar'
 
 
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Navbar from "../Components/Navbar";
+import Navbar from "../components/Navbar";
 
 // import Font Awesome CSS
 
