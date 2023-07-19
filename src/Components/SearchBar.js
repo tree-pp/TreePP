d4bd969f5de7fa658742c576f62409f28952d253
moudud99 <moudud99@gmail.com>
2023-07-19 21:57:24 +0600

    fixing routing

---
+++ src/Components/SearchBar.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+const SearchBar = () => {
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearch = () => {
+    // Perform search operation using the searchText
+    console.log('Search Text:', searchText);
+  };
+
+  const handleChange = (event) => {
+    setSearchText(event.target.value);
+  };
+
+  return (
+    <div className="flex items-stretch pt-5">
+      <input
+        type="text"
+        placeholder="Type tree ID here ..."
+        value={searchText}
+        onChange={handleChange}
+        className="placeholder:font-light placeholder:text-slate-300 text-sm py-2 px-4 rounded-l-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+      />
+      <button
+        type="button"
+        onClick={handleSearch}
+        className="text-sm bg-slate-400 hover:bg-slate-500 text-white px-4 rounded-r-lg"
+      >
+        Search
+      </button>
+    </div>
+  );
+};
+
+export default SearchBar;
---
+++ src/app/tree-details/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+import SearchBar from '../../components/SearchBar';
+import TreeDetails from '../../components/treeDetails';
+import Image from "next/image";
+
+// const treeProps={
+//     id: '000182',
+//     name: 'Swietenia Mahogoni',
+//     datePlanted: '000182',
+//     donor: 'Mr. X',
+//     landOwner: 'Mr. Y',
+//     location: 'Gilatala, Khulna',
+//     landType: 'Household Yard',
+//     image: './tree.jpeg'
+// }
+
+export default function tree_details() {
+    return (
+<div className='flex flex-col justify-center items-center'>
+             <SearchBar />
+        <TreeDetails 
+        id='000182'
+        name='Swietenia Mahogoni'
+        datePlanted='000182'
+        donor='Mr. X'
+        landOwner='Mr. Y'
+        location='Gilatala, Khulna'
+        landType='Household Yard'
+        image='./tree.jpeg'
+        />
+    </div>
+    )
+    }
