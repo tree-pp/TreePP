0d9aef415c0a8ec32cb1e740151c3fbb3d9e1937
sanz47 <syemaziz@iut-dhaka.edu>
2023-08-15 18:09:53 +0600

    minor change in heading

--- src/components/WhatWeDo.tsx
+++ src/components/WhatWeDo.tsx
@@ -29,7 +29,7 @@ const WhatWeDo = () => {
           variants={variants}
           transition={{ duration: 0.5 }}
         >
-          Who We Are
+          What We Do
           <div className="absolute bottom-0 left-0 w-1/2 h-1 bg-green-500 transform rounded-lg translate-y-1"></div>
         </motion.h1>
         <motion.p
