c19109012b13dc1490d713c6ee398a1d951c6d2e
sanz47 <syemaziz@iut-dhaka.edu>
2023-09-23 12:41:30 +0600

    Slight changes in SlideshowCard

--- src/components/SlideshowCard.js
+++ src/components/SlideshowCard.js
@@ -3,10 +3,10 @@ import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 
 const cardData = [
-    { src: '/tree.png', name: 'Firstname Lastname', quote: 'Tree++ is a very nice initiative.I am very happy to be a part of it. I stayed with them since their begining.' },
-    { src: '/tree.png', name: 'Oldname Newname', quote: 'Tree++ is a very nice initiative.I like it. I stayed with them since their begining.' },
-    { src: '/tree.png', name: 'Name1 Name2', quote: 'Tree++ is a very nice initiative.I want it. I stayed with them since their begining.' },
-    { src: '/tree.png', name: 'Your Name', quote: 'Tree++ is a very nice initiative. Text, more text even more text and many more text. I love it. I stayed with them since their begining.' },
+    { src: '/tree.png', name: 'Firstname Lastname', quote: 'Tree++ is a very nice initiative. I am very happy to be a part of it. I stayed with them since their beginning.' },
+    { src: '/tree.png', name: 'Oldname Newname', quote: 'Tree++ is a very nice initiative. I like it. I stayed with them since their beginning.' },
+    { src: '/tree.png', name: 'Name1 Name2', quote: 'Tree++ is a very nice initiative. I want it. I stayed with them since their beginning.' },
+    { src: '/tree.png', name: 'Your Name', quote: 'Tree++ is a very nice initiative. Text, more text even more text and many more text. I love it. I stayed with them since their beginning.' },
 ];
 
 export default function SlideshowCard() {
@@ -34,31 +34,36 @@ export default function SlideshowCard() {
     };
 
     return (
-
-        <div className="flex flex-col md:items-stretch py-10 container mx-auto ">
+        <div className="flex flex-col py-10 container mx-auto">
             <div className="head-text text-center">
                 <span>What Our <b>Advisors</b> Say</span>
                 <div className="absolute bottom left-1/2 transform -translate-x-1/2 translate-y-1/2 h-1 bg-green-500 w-20"></div>
             </div>
             <div>
-                <div className="flex flex-col md:flex-row md:justify-between mt-5 items-center md:w-full md:px-32">
-                <div className={`flex flex-col md:flex-row pt-5 md:w-5/12 md:justify-start items-center transition-opacity ${isEntering ? 'opacity-100' : 'opacity-0'}`}>
-
-                        <Image src={cardData[currentCard].src} width={200} height={200} alt="advisor" className="rounded-full pl-25 bg-yellow-300"></Image>
-                        <div className="flex flex-col pl-10 ">
-                        <div className={`sub-head-text pt-10 text-center transition-opacity ${isEntering ? 'opacity-100' : 'opacity-0'}`}>
+                <div className="flex flex-col md:flex-row md:justify-between mt-5 items-center lg:w-full lg:px-8">
+                    <div className={`flex flex-col md:flex-row pt-4 md:w-full md:justify-center items-center transition-opacity ${isEntering ? 'opacity-100' : 'opacity-0'}`}>
+                        <Image src={cardData[currentCard].src} width={200} height={200} alt="advisor" className="rounded-full pl-25 bg-yellow-300" />
+                        <div className="flex flex-col md:pl-10 text-center">
+                            <div className={`sub-head-text mt-4 transition-opacity ${isEntering ? 'opacity-100' : 'opacity-0'}`}>
                                 {cardData[currentCard].name}
                             </div>
                             <span className={`caption-text transition-opacity ${isEntering ? 'opacity-100' : 'opacity-0'}`}>Big guy at big company</span>
                         </div>
                     </div>
-                    <div className={`paragraph-text md:w-7/12 p-10 md:rounded-xl text-start transition-opacity ${isEntering ? 'opacity-100' : 'opacity-0'}`} >
+                    <div className={`paragraph-text md:w-full pl-8 pr-6 pt-10 lg:pt-6 lg:pr-20 text-center transition-opacity ${isEntering ? 'opacity-100' : 'opacity-0'}`}>
                         <div style={{ whiteSpace: 'pre-wrap' }}>
-                            {cardData[currentCard].quote}
+                            <div class="max-w-4xl p-4 text-gray-800 bg-green-200 rounded-lg shadow transition-opacity" style={{ opacity: isEntering ? 1 : 0 }}>
+                                <div class="mb-2">
+                                    <div class="h-3 text-3xl text-left text-gray-600">“</div>
+                                    <p class="px-4 text-sm text-center italic font-serif text-gray-600">
+                                        {cardData[currentCard].quote}
+                                    </p>
+                                    <div class="h-3 text-3xl text-right text-gray-600">”</div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-
             </div>
             <div className="flex justify-center mt-2">
                 {cardData.map((_, index) => (
