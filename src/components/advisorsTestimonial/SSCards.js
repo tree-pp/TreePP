89a647ca060210459f69bf141ea4ef6eff1d7c34
Mostafiz <mostafiz93@gmail.com>
2023-10-15 02:55:02 +0600

    upd> advisors testimonial updated

--- src/components/advisorsTestimonial/SSCards.js
+++ src/components/advisorsTestimonial/SSCards.js
@@ -4,55 +4,52 @@ import Image from 'next/image';
 
 export default function SSCards({ isEntering, cardData, currentCard }) {
 	return (
-		<div>
-			<div className='flex flex-col md:flex-row md:justify-between mt-5 items-center lg:w-full lg:px-8'>
-				<div
-					className={`flex flex-col md:flex-row pt-4 md:w-full md:justify-center items-center transition-opacity duration-1000 ${
-						isEntering ? 'opacity-100' : 'opacity-0'
-					}`}
-				>
+		<div className='flex flex-col  justify-center'>
+			<div
+				className={`flex flex-row  pt-10 -mb-3  justify-center  transition-opacity duration-1000 ${
+					isEntering ? 'opacity-100' : 'opacity-0'
+				}`}
+			>
+				<div className='h-20 w-20 relative'>
 					<Image
 						src={cardData[currentCard].src}
-						width={200}
-						height={200}
 						alt='advisor'
-						className='rounded-full pl-25 bg-yellow-300'
+						className='rounded-full p-2 '
+						layout='fill'
 					/>
-					<div className='flex flex-col md:pl-10 text-center'>
-						<div
-							className={`sub-head-text mt-4 transition-opacity duration-1000 ${
-								isEntering ? 'opacity-100' : 'opacity-0'
-							}`}
-						>
-							{cardData[currentCard].name}
-						</div>
-						<span
-							className={`caption-text transition-opacity duration-1000 ${
-								isEntering ? 'opacity-100' : 'opacity-0'
-							}`}
-						>
-							Big guy at big company
-						</span>
-					</div>
 				</div>
-				<div
-					className={`paragraph-text md:w-full pl-8 pr-6 pt-10 lg:pt-6 lg:pr-20 text-center transition-opacity duration-1000 ${
-						isEntering ? 'opacity-100' : 'opacity-0'
-					}`}
-				>
-					<div style={{ whiteSpace: 'pre-wrap' }}>
-						<div
-							class='max-w-4xl p-4 text-gray-800 bg-amber-100 rounded-lg shadow transition-opacity duration-1000'
-							style={{ opacity: isEntering ? 1 : 0 }}
-						>
-							<div class='mb-2'>
-								<div class='h-5 text-3xl italic text-left text-gray-600'>“</div>
-								<p class='px-4 text-lg text-center italic font-serif text-gray-600'>
-									{cardData[currentCard].quote}
-								</p>
-								<div class='h-4 text-3xl italic text-right text-gray-600'>
-									”
-								</div>
+				<div className='flex flex-col pl-2 justify-start'>
+					<p
+						className={`font-sans Roboto text-lg mt-4 transition-opacity duration-500 ${
+							isEntering ? 'opacity-100' : 'opacity-0'
+						}`}
+					>
+						{cardData[currentCard].name}
+					</p>
+					<p
+						className={`text-sm text-gray-500 transition-opacity duration-500 ${
+							isEntering ? 'opacity-100' : 'opacity-0'
+						}`}
+					>
+						Big guy at big company
+					</p>
+				</div>
+			</div>
+			<div className='flex justify-center'>
+				<div style={{ whiteSpace: 'pre-wrap' }}>
+					<div
+						class='max-w-4xl p-4 text-gray-800 bg-amber-0 rounded-lg shadow transition-opacity duration-1000'
+						style={{ opacity: isEntering ? 1 : 0 }}
+					>
+						<div class='mb-2'>
+							<div class='-mb-12 text-7xl italic text-left text-gray-400'>
+								“
+							</div>
+							<p class='indent-6 px-4 text-lg text-center italic font-serif text-gray-600'>
+								{cardData[currentCard].quote}
+							</p>
+							<div class='-mb-12 -mt-1 -mr-0 text-7xl italic text-right text-gray-300'>
+								”
 							</div>
 						</div>
 					</div>
