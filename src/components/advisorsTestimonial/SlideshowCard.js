2f71d42c27f44075e37c9fd604afc08a4b47c1ea
Mostafiz <mostafiz93@gmail.com>
2023-10-15 02:22:58 +0600

    upd> random images in dummy data

--- src/components/advisorsTestimonial/SlideshowCard.js
+++ src/components/advisorsTestimonial/SlideshowCard.js
@@ -7,25 +7,25 @@ import SSCards from './SSCards';
 
 const cardData = [
 	{
-		src: '/tree.png',
+		src: 'https://media.licdn.com/dms/image/D4D03AQGuSjG2_tYlxQ/profile-displayphoto-shrink_400_400/0/1668714950341?e=1702512000&v=beta&t=ZC635ZFJK8RCtnfL46-nkKB4c7zR04jUL1NYIZ7HSTE',
 		name: 'Firstname Lastname',
 		quote:
 			'Tree++ is a very nice initiative. I am very happy to be a part of it. I stayed with them since their beginning.'
 	},
 	{
-		src: '/tree.png',
+		src: 'https://media.licdn.com/dms/image/D5603AQH6oA8v7uez3A/profile-displayphoto-shrink_100_100/0/1676816043087?e=1702512000&v=beta&t=NVxXY-dUQLdXSJTeaZikEKt-bPhC7rdjPuhVOE83HY8',
 		name: 'Oldname Newname',
 		quote:
 			'Tree++ is a very nice initiative. I like it. I stayed with them since their beginning.'
 	},
 	{
-		src: '/tree.png',
+		src: 'https://media.licdn.com/dms/image/C4D03AQFM3Y2r-OEStw/profile-displayphoto-shrink_100_100/0/1515623804434?e=1702512000&v=beta&t=FRAA29d66Nj5i7qsL-JNlyZYfWDOMx9UPtzDfKE38wI',
 		name: 'Name1 Name2',
 		quote:
 			'Tree++ is a very nice initiative. I want it. I stayed with them since their beginning.'
 	},
 	{
-		src: '/tree.png',
+		src: 'https://media.licdn.com/dms/image/C5603AQGBG-pWCdQcIA/profile-displayphoto-shrink_100_100/0/1520369865349?e=1702512000&v=beta&t=vPRY77uHungePGWW6eudRQv2AS_-9jgnHjheH4n5GlE',
 		name: 'Your Name',
 		quote:
 			'Tree++ is a very nice initiative. Text, more text even more text and many more text. I love it. I stayed with them since their beginning.'
